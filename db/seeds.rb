0910f599341f41e8d7d27043826701469ff81079
Fiorella Larriviere <fcastillolarriviere@gmail.com>
2022-10-13 21:47:56 -0300

    seed usuarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,148 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# puts "creando usuario 1"
+
+# User.create(
+#   first_name: "Ana",
+#   last_name: "Perez",
+#   phone_number: "989898898",
+#   ruc: "989898898000",
+#   dni: "98989880",
+#   address: "Av. Larco 1711",
+#   city: "Trujillo",
+#   latitude: "",
+#   longitude: "",
+#   email: "hola@gmail.com",
+#   password: "123456"
+# )
+
+# puts "usuario 1 creado"
+
+# puts "creando usuario 2"
+
+# User.create(
+#   first_name: "Juan",
+#   last_name: "Díaz",
+#   phone_number: "46644646",
+#   ruc: "466446464444",
+#   dni: "46644648",
+#   address: "Av. El Golf 3114",
+#   city: "Trujillo",
+#   latitude: "",
+#   longitude: "",
+#   email: "jd@gmail.com",
+#   password: "123456"
+# )
+
+# puts "usuario 2 creado"
+
+# puts "creando usuario 3"
+
+# User.create(
+#   first_name: "Jhon",
+#   last_name: "Smith",
+#   phone_number: "49994646",
+#   ruc: "466446464404",
+#   dni: "46677648",
+#   address: "Av. Flora Tristan 324",
+#   city: "Lima",
+#   latitude: "",
+#   longitude: "",
+#   email: "js@gmail.com",
+#   password: "123456"
+# )
+
+# puts "usuario 3 creado"
+
+# puts "creando usuario 4"
+
+# User.create(
+#   first_name: "Alexandra",
+#   last_name: "Jimenez",
+#   phone_number: "76556776",
+#   ruc: "765567765374",
+#   dni: "46644827",
+#   address: "Av. El Golf 555",
+#   city: "Lima",
+#   latitude: "",
+#   longitude: "",
+#   email: "aj@gmail.com",
+#   password: "123456"
+# )
+
+# puts "usuario 4 creado"
+
+# puts "creando usuario 5"
+
+# User.create(
+#   first_name: "Maria",
+#   last_name: "Romero",
+#   phone_number: "46987646",
+#   ruc: "4664464000044",
+#   dni: "46622648",
+#   address: "Calle Geranios 3114",
+#   city: "Arequipa",
+#   latitude: "",
+#   longitude: "",
+#   email: "mrom@gmail.com",
+#   password: "1234567"
+# )
+
+# puts "usuario 5 creado"
+
+# puts "creando usuario 6"
+
+# User.create(
+#   first_name: "Adrian",
+#   last_name: "Bello",
+#   phone_number: "46364646",
+#   ruc: "4092388444",
+#   dni: "43434548",
+#   address: "Av. El Sol 114",
+#   city: "Cusco",
+#   latitude: "",
+#   longitude: "",
+#   email: "abello@gmail.com",
+#   password: "123456"
+# )
+
+# puts "usuario 6 creado"
+
+
+puts "creando categoria 1"
+Category.create(
+  name: "Maquillaje"
+)
+puts "Catergory 1 creada"
+
+puts "creando categoria 2"
+Category.create(
+  name: "Abarrotes"
+)
+puts "Catergory 2 creada"
+
+puts "creando categoria 3"
+Category.create(
+  name: "Electrónicos"
+)
+puts "Catergory 3 creada"
+
+puts "creando categoria 4"
+Category.create(
+  name: "Cuidado personal"
+)
+puts "Catergory 4 creada"
+
+puts "creando categoria 5"
+Category.create(
+  name: "Libreria"
+)
+puts "Catergory 5 creada"
+
+puts "creando categoria 6"
+Category.create(
+  name: "Juguetes"
+)
+puts "Catergory 6 creada"
