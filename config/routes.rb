9f1eb557cb0599f856b3427a6b9cdae85e2d1c9f
Eryck Milano <eryckmilano@gmail.com>
2022-11-01 20:37:49 -0300

    cambios en vista orders, y carrito

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'line_items/:id/add' => "line_items#add_quantity", as: "line_item_add"
   get 'line_items/:id/reduce' => "line_items#reduce_quantity", as: "line_item_reduce"
   post 'line_items' => "line_items#create"
-  delete 'line_items/:id' => "line_items#destroy"
+  delete 'line_items/:id' => "line_items#destroy", as: "line_item_erase"
 
   resources :orders, only: [:index, :show, :new, :create]
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
