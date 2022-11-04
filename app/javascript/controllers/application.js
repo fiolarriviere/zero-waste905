7dc7bcb777b839a6c9a07d34e880454e1d515a95
Eryck Milano <eryckmilano@gmail.com>
2022-11-03 22:36:55 -0300

    stimulus

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -3,6 +3,10 @@ import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 
 // Configure Stimulus development experience
+
+import { Slideover } from "tailwindcss-stimulus-components"
+application.register('slideover', Slideover)
+
 application.debug = false
 window.Stimulus   = application
 
