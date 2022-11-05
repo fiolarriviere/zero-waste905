0d36efb3e7ced630bd8fbb247999d500fafb1dcb
Fiorella Larriviere <fcastillolarriviere@gmail.com>
2022-11-05 13:56:39 -0300

    cambios photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :current_cart
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :ruc, :dni, :business_name, :photo, :address, :city])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :ruc, :dni, :business_name, :photo, :address, :city])
+  end
+
   private
 
   def current_cart
@@ -11,11 +19,3 @@ class ApplicationController < ActionController::Base
     session[:cart_id] = @current_cart.id
   end
 end
-
-# before_action :configure_permitted_parameters, if: :devise_controller?
-#   def configure_permitted_parameters
-
-#     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-
-#     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :photo])
-#   end
