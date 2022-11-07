1162c1ec8885980867a9ccc42b1ee78ce3bc4f90
Jean Pierre <jp.saldana@outlook.es>
2022-11-07 17:32:29 -0500

    validacion usuario index y show sin LOGEARSE

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_products, only: %w[show edit update destroy]
+  skip_before_action :authenticate_user!, execpt: %i[index show]
 
   def index
     if params[:query].present?
