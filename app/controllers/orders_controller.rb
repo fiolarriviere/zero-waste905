f841c364c154e90ef93ee9004390ee6d16afae8a
Jean Pierre <jp.saldana@outlook.es>
2022-11-12 10:48:59 -0500

    Finito

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,7 +20,7 @@ class OrdersController < ApplicationController
     @order.save
     Cart.destroy(session[:cart_id])
     session[:cart_id] = nil
-    redirect_to orders_path
+    redirect_to order_path(current_user)
   end
 
   private
